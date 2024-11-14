1ed5b0ce8381102df135f1af3db98f303218b4ea
iqbal <93726487+keep-xylent@users.noreply.github.com>
2024-11-14 12:49:42 +0700

    jumlah malam

--- src/PemesananKamar.java
+++ src/PemesananKamar.java
@@ -1,13 +1,20 @@
+import java.util.Date;
+
 public class PemesananKamar {
     Kamar kamar;
-    int jmlMalam;
+    Date checkIn;
+    Date checkOut;
 
-    public PemesananKamar(Kamar kamar, int jmlMalam) {
+    public PemesananKamar(Kamar kamar, Date checkIn, Date checkOut) {
         this.kamar = kamar;
-        this.jmlMalam = jmlMalam;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
     public int hitungTotalHarga() {
+        int diffInMillis = (int)(checkOut.getTime() - checkIn.getTime());
+        int jmlMalam = diffInMillis / (1000 * 60 * 60 * 24);
+        
         return kamar.hargaPerMalam * jmlMalam;
     }
 }
